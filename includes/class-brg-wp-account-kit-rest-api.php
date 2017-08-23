61caf33fe4ee4f68b82582634259d916b40a4ed4
Jonathan A. Schweder <jonathanschweder@gmail.com>
2017-08-22 22:32:09 -0300

    corrected redirect to admin page after login

--- includes/class-brg-wp-account-kit-rest-api.php
+++ includes/class-brg-wp-account-kit-rest-api.php
@@ -56,13 +56,21 @@ class Brg_Wp_Account_Kit_REST_API {
 
 	//the route that will receive return data from facebook
 	public function return_function(){
-		//TODO verify nonce in state field
+        if (!isset($_GET['status'])) {
+            wp_die('<strong>Account Kit Error</strong>: "status" not informed', 'Account Kit');
+        }
+
 		switch ($_GET['status']){
 			case 'PARTIALLY_AUTHENTICATED' :
-				$token_exchange_url = 'https://graph.accountkit.com/'.$this->app_data['api_version'].'/access_token?'.
-				  'grant_type=authorization_code'.
-				  '&code='.$_GET['code'].
-				  "&access_token=AA|".$this->app_data['app_id']."|".$this->app_data['app_secret'];
+                $token_exchange_url = 'https://graph.accountkit.com/'
+                    . $this->app_data['api_version']
+                    . '/access_token?'
+                    . 'grant_type=authorization_code'
+                    . '&code='.$_GET['code']
+                    . "&access_token=AA|"
+                    . $this->app_data['app_id']
+                    . "|"
+                    . $this->app_data['app_secret'];
 				$data = $this->doCurl($token_exchange_url);
 
 				//data returned
@@ -72,41 +80,45 @@ class Brg_Wp_Account_Kit_REST_API {
 
 				// Get Account Kit information
 				$appsecret_proof = hash_hmac('sha256', $user_access_token, $this->app_data['app_secret']);
-				$me_endpoint_url = 'https://graph.accountkit.com/'.$this->app_data['api_version'].'/me?'.
-				  'access_token='.$user_access_token.'&appsecret_proof='.$appsecret_proof;
+                $me_endpoint_url = 'https://graph.accountkit.com/'
+                    . $this->app_data['api_version'].'/me?'
+                    . 'access_token='
+                    . $user_access_token
+                    . '&appsecret_proof='
+                    . $appsecret_proof;
 				$data = $this->doCurl($me_endpoint_url);
 				$phone = isset($data['phone']) ? $data['phone']['number'] : '';
 				$email = isset($data['email']) ? $data['email']['address'] : '';
 				$username = 'ak_'.$user_id;
 
 				//check if user exists
-				$uid = username_exists($username);
-				$user_email = email_exists($email);
+				$uid = email_exists($email);
+                if (!$uid) {
+                    $uid = username_exists($username);
+                }
+
 				if ($uid){
 					//this user already authenticated through Account Kit API
 					//let's update the access_token
 					update_user_meta($uid,'_brg_wp_account_kit_token', $user_access_token);
-				}elseif ($user_email){
-					//this user email is already registered but not authenticated though Account Kit API
-					//let's update the access_token
-					update_user_meta($uid,'_brg_wp_account_kit_token', $user_access_token);
 				}else{
 					//first time login for this user
-					$uid = wp_create_user($username, $user_access_token, $email );
+					$uid = wp_create_user($uid, $user_access_token, $email );
 				}
 				//everything is working great! So let's set auth cookie and redirect user to admin_url();
+                wp_set_current_user($uid, $user_login);
 				wp_set_auth_cookie($uid);
-				wp_redirect(admin_url());
-                exit();
+                wp_redirect( admin_url( 'index.php' ) );
+                exit;
 			break;
 			case 'NOT_AUTHENTICATED' :
-				wp_die();
+                wp_die('<strong>Account Kit Error</strong>: not authenticated', 'Account Kit');
 			break;
 			case 'BAD_PARAMS' :
-				wp_die();
+                wp_die('<strong>Account Kit Error</strong>: bad params', 'Account Kit');
 			break;
 			default :
-				wp_die();
+                wp_die('<strong>Account Kit Error</strong>: invalid "status"', 'Account Kit');
 			break;
 		}
 
