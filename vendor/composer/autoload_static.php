c8975f32c5b7bc9e1e825a499db72a426f21fbd2
RIcardo <ricardo@brgweb.com.br>
2017-10-31 06:13:55 -0200

    packing for WP plugin

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitc1be8f31564f468ae9378d1ab3a9460e
+class ComposerStaticInit6d0ac315db05dfa49f5df9f84b7c21d5
 {
     public static $files = array (
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
@@ -101,10 +101,10 @@ class ComposerStaticInitc1be8f31564f468ae9378d1ab3a9460e
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitc1be8f31564f468ae9378d1ab3a9460e::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitc1be8f31564f468ae9378d1ab3a9460e::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitc1be8f31564f468ae9378d1ab3a9460e::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInitc1be8f31564f468ae9378d1ab3a9460e::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit6d0ac315db05dfa49f5df9f84b7c21d5::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit6d0ac315db05dfa49f5df9f84b7c21d5::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit6d0ac315db05dfa49f5df9f84b7c21d5::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit6d0ac315db05dfa49f5df9f84b7c21d5::$classMap;
 
         }, null, ClassLoader::class);
     }
